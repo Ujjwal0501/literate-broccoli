4964751bdb6b2cbde030cf8a4beb3fad5cc2c518
Ujjwal Kumar <31813384+Ujjwal0501@users.noreply.github.com>
2023-12-19 03:12:41 +0530

    Add new routes & templates

--- server/routes.py
+++ server/routes.py
@@ -16,6 +16,12 @@ root_key, new = UserSession.get_or_create(session_id='13717e9b-5406-4839-a184-b2
 root_key.update(quota_left=4294967296).execute()
 
 
+@app.route('/', methods=['GET'])
+def render_home():
+    route_list = [{"name": "Barcode Generator", "path": "barcode"}, {"name": "Qrcode Generator", "path": "qrcode"}, {"name": "SKU Auto Generator", "path": "sku-generator"}, {"name": "Customized SKU Generator", "path": "customized-sku-generator"}]
+    return render_template('home.html', route_list=route_list), 200 
+
+
 @server.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -31,6 +37,16 @@ def barcode_page():
 def qrcode_page():
     handle_visit(session)
     return render_template('qrcode.html'), 200     
+
+
+@app.route('/sku-generator', methods=['GET'])
+def sku_page():
+    return render_template('skuauto.html'), 200     
+
+
+@app.route('/customized-sku-generator', methods=['GET'])
+def custom_sku_page():
+    return render_template('skucustomized.html'), 200     
 
 
 if __name__ == '__main__':
