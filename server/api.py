052c661ea3480687911e3ca55d4bd889f5d3fadb
Ujjwal Kumar <31813384+Ujjwal0501@users.noreply.github.com>
2023-12-19 23:14:27 +0530

    fix api

--- server/api.py
+++ server/api.py
@@ -4,7 +4,8 @@ from activity import validate_query_src, handle_qrcode_proc, handle_barcode_proc
 
 from flask import request, send_file, Blueprint, session
 from config import MAX_REQUEST_SIZE
-import csv, io, json
+import csv, io, json, peewee
+from hashlib import sha1
 
 
 api = Blueprint("api", __name__)
@@ -21,14 +22,20 @@ def handle_barcode_reqeust():
     if file.filename == '' or not file:
         return 'No file selected for uploading', 400
 
+    task = None
     stream = io.StringIO(file.stream.read().decode("UTF8"), newline=None)
     data = list(csv.reader(stream))
     data = [item for sublist in data for item in sublist]
     if len(data) > MAX_REQUEST_SIZE:
         return f'Entry limit exceeded. Maximum entry allowed is {MAX_REQUEST_SIZE}', 400
 
-    task = Task.create(entry_count = len(data), data_entries = ','.join(data))
-    handle_barcode_proc.delay(task.task_id, task.data_entries)
+    try:
+        task = Task.create(entry_count = len(data), data_entries = ','.join(data))
+    except peewee.IntegrityError as e:
+        task = Task.get_or_none(task_id = sha1(bytes(','.join(data), 'utf-8')).hexdigest())
+    finally:
+        handle_barcode_proc.delay(task.task_id, task.data_entries)
+        
 
     if user:
         user.update(quota_left=user.quota_left-1).execute()
@@ -54,8 +61,12 @@ def handle_qrcode_reqeust():
     if len(data) > MAX_REQUEST_SIZE:
         return f'Entry limit exceeded. Maximum entry allowed is {MAX_REQUEST_SIZE}', 400
 
-    task = Task.create(entry_count = len(data), data_entries = ','.join(data))
-    handle_qrcode_proc.delay(task.task_id, task.data_entries)
+    try:
+        task = Task.create(entry_count = len(data), data_entries = ','.join(data))
+    except peewee.IntegrityError as e:
+        task = Task.get_or_none(task_id = sha1(bytes(','.join(data), 'utf-8')).hexdigest())
+    finally:
+        handle_qrcode_proc.delay(task.task_id, task.data_entries)
 
     if user:
         user.update(quota_left=user.quota_left-1).execute()
